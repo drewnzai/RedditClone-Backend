cf9607a7eeaaeb7e393c5ea79eb1ac3326cd74f4
Andrew <andrewnzaikombe@gmail.com>
2022-02-27 18:08:57 +0300

    New Model classes added.

---
+++ src/main/java/com/drew/Reddit/models/Comment.java
@@ -0,0 +1,32 @@
+package com.drew.Reddit.models;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotEmpty;
+
+import java.time.Instant;
+
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    private String text;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "postId", referencedColumnName = "postId")
+    private Post post;
+
+    private Instant createdDate;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+}
