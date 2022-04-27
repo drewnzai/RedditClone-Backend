aadf66dd3fa1800def497641c545d2ea255f4add
Andrew <andrewnzaikombe@gmail.com>
2022-04-28 01:10:31 +0300

    Hmmm.

---
+++ src/main/java/com/drew/Reddit/api/AuthController.java
@@ -0,0 +1,15 @@
+package com.drew.Reddit.api;
+
+import com.drew.Reddit.utils.RegisterRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @PostMapping
+    public void signup(@RequestBody RegisterRequest request){}
+}
