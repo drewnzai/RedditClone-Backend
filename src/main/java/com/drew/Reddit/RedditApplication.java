8b7bb624f9a1abadd26a8c8c6f14ffb718ed7afd
Andrew <andrewnzaikombe@gmail.com>
2022-08-31 15:15:37 +0300

    Swagger issue

--- src/main/java/com/drew/Reddit/RedditApplication.java
+++ src/main/java/com/drew/Reddit/RedditApplication.java
@@ -2,17 +2,13 @@ package com.drew.Reddit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+
 @SpringBootApplication
-//@EnableAsync
-//@ComponentScan("com.drew.Reddit.mapper")
-
-// I have commented out the above component scan since it is
-// causing the app to use default security config rather than our own implementation
-// Also, @ComponentScan is already implemented in the SpringbootApplication class
-
+@EnableAsync
+@Import(SwaggerConfiguration.class)
 public class RedditApplication {
 
 	public static void main(String[] args) {
