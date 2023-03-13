dcd04e29af03fc44dc7f3f73a31409ddbc6053d6
Deusnoire <andrewnzaikombe@gmail.com>
2023-03-13 15:22:05 +0300

    Added new API, cleaned trending-items.ts

--- src/Frontend/src/app/models/trending-items.ts
+++ src/Frontend/src/app/models/trending-items.ts
@@ -1,13 +1,7 @@
 
-export interface trendingItems{
+export interface TrendingItems{
     image_src: string;
     title: string;
-    description: string;
-    subreddit: Subreddit; 
-}
-
-interface Subreddit{
-    image_src: string;
-    name: string;
-
+    subreddit_description: string;
+    subreddit_name: string;
 }
--- src/Frontend/src/app/services/trending-items.service.ts
+++ src/Frontend/src/app/services/trending-items.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { TrendingItems } from '../models/trending-items';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrendingItemsService {
 
-  constructor(private http: HttpClient) { }
+constructor(private http: HttpClient) { }
 
-  
+  getTrendingItems(): Observable<Array<TrendingItems>>{
+    
+    return this.http.get<Array<TrendingItems>>("http://localhost:8080/api/trending-items");
+  }
+
 }
